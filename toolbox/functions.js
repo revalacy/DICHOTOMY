7f5477db9f416f3b42e65a22e4e8887a99d453f7
revalacy <41456629+revalacy@users.noreply.github.com>
2018-11-05 23:09:12 -0800

    Update functions.js

--- toolbox/functions.js
+++ toolbox/functions.js
@@ -14,6 +14,7 @@ const GoogleSpreadsheet = require('google-spreadsheet');
 //Remember that all async functions resolving promises must have their function variables declared as local variables BEFORE the Promise object
 
 var efn = require('./effects.js');
+var fn = require('./functions.js');
 
 
 module.exports =  {
@@ -36,7 +37,7 @@ setval: async function(a, b, c, d){//sets values to core database, accepts argum
      let searchval = b;
      let keys = c;
      let values = d;
-   doc.getRows(index, {'query': 'label = "' + searchval + '"'},function(err,row){
+     doc.getRows(index, {'query': 'label = "' + searchval + '"'},function(err,row){
       if (!row) return console.log('An error occurred. Terminating function setval.')
       for (var i = 0; i < keys.length; i++){
         let key = keys[i];
@@ -236,18 +237,21 @@ querydoc: async function(a, b, c, d) {//queries the doc using provided index, qu
 
   eventsetup: async function(a){
     var searchval = a;
-    var x = [];
-    var keys = ['label','title','starttime','gms','room','set','platform','blurb'];
-   
-      eventdoc.getRows(1,{'query':'label = ' + searchval + '"'},function(err,row){
-        if (!row){x = 'none'; console.log('An error has occurred in the process of pulling event data for setup. Function eventsetup terminated.'); return resolve(x)};
-        for (var i = 0; i < keys.length; i++){
-          var key = key[i];
+    var x = ['registration', '', '', '', '', '', '', '', '', ''];
+    var keys = ['state', 'label','title','starttime','gms','room','set','platform','blurb', 'meetup'];
+    return new Promise(resolve => {
+      eventdoc.getRows(1,{'query':'label = "' + searchval + '"'},function(err,row){
+        if (!row) return console.log('An error has occurred in the process of pulling event data for setup. Function eventsetup terminated.');
+        for (var i = 1; i < keys.length; i++){
+          var key = keys[i];
           x[i] = row[0][key];
         };
-        this.setval(7,'open',keys,x);
+        console.log('Event setup complete.');
+        resolve (x);
       });
     
+    });
+      
   },
 
   eventopencheck: async function(a){
@@ -255,12 +259,14 @@ querydoc: async function(a, b, c, d) {//queries the doc using provided index, qu
       var keys = ['label','title','starttime','gms','room','set','platform','blurb'];
       var x = 0;
       return new Promise(resolve =>{
-        eventdoc.getRows(1,{'query':'label = ' + searchval + '"'},function(err,row){
-          if (!row){x = 'none'; console.log('An error has occurred in the process of pulling event data for setup. Function eventsetup terminated.'); return resolve(x)};
+        eventdoc.getRows(1,{'query':'label = "' + searchval + '"'},function(err,row){
+          if (!row){x = 'none'; console.log('An error has occurred in the process of pulling event data for setup. Function eventopencheck terminated.'); return resolve(x)};
           for(var i = 0; i < keys.length; i++){
-            key = keys[i];
+            var key = keys[i];
+            if (key === '') continue;
             var y = 0;
-            if (row[0][key] === '') y = -1;
+            var val = row[0][key];
+            if (val  === '') y = -1;
             x += y;
           };
           resolve (x);
@@ -270,19 +276,229 @@ querydoc: async function(a, b, c, d) {//queries the doc using provided index, qu
 
   eventstartcheck: async function(a){
       var searchval = a;
-      var keys = ['title', 'state','starttime','gms','room','set','platform','npcs','startmsg','endmsg','roundmessages'];
+      var keys = ['title', 'state','starttime','gms','room','set','platform','npcs','startmsg','endmsg','roundmessages','meetup'];
       var x = 0;
       return new Promise(resolve => {
-        doc.getRows(7,{'query':'label = ' + searchval + '"'},function(err,row){
+        doc.getRows(7,{'query':'label = "' + searchval + '"'},function(err,row){
+            if(!row){x = 'none'; console.log('An error occurred in the process of running the start check for an event command. Function eventstartcheck terminated.');return resolve(x)};
             for (var i = 0; i < keys.length; i++){
               var key = keys[i];
               var y = 0;
-              if (row[0][key] === '') y = -1;
+              if (row[0][key] === '' || row[0][key] === 'TBD') y = -1;
               x += y;
             };
             resolve (x);
         });
       });
   },
+
+  eventcreationclear: async function(a){
+      var searchval = a;
+      var keys = ['label',	'title',	'starttime',	'gms',	'room',	'set',	'platform',	'blurb', 'meetup'];
+      eventdoc.getRows(1, {'query':'label = "' + searchval + '"'},function(err,row){
+          if (!row)return console.log('An error occurred in the process of removing event data for setup. Function eventcreationclear terminated.');
+          for (var i = 0; i < keys.length; i++){
+            var key = keys[i];
+            row[0][key] = '';
+          };
+          row[0].save();
+      });
+  },
+
+  roomquery: async function(a){
+      var roomstring = a;
+      return new Promise(resolve =>{
+        doc.getRows(7,{'query':'room = ' + roomstring + '" and state = "active"'},function(err,row){
+          var x = 1;
+          if (!row) x = 0
+          resolve (x)
+        });
+      });
+      
+  },
+
+  ingamemsgparser: async function(a, b){
+    var messagestring = a;
+    messagestring = messagestring.replace(/\r?\n|\r/g,' ').replace('  ', ' ');
+    var totalchars = messagestring.length;
+    var sentchars = 0;
+    var startchar = 0;
+    var endchar = 0;
+    var msgarray = [];
+    var msgnum = 0;
+    var numberfiller = ('');
+    var charlength = 0;
+    var maxchars = 486;
+    var msgindex = 0;
+    var preappend = b;
+
+  while (sentchars < totalchars ){
+    var maxend = startchar + maxchars;
+    var initialmsgstring = messagestring.substring(startchar, maxend);
+    var lastperiod = initialmsgstring.lastIndexOf(".");
+    var lastquestion = initialmsgstring.lastIndexOf("?");
+    var lastexclaim = initialmsgstring.lastIndexOf("!");
+    var lastquotes = initialmsgstring.lastIndexOf('"');
+    var endindex = Math.max(lastperiod,lastquestion,lastexclaim,lastquotes);
+    if (maxend >= totalchars) endindex = maxend;
+    endchar = +endindex + +startchar + 1;
+    var msgstring = messagestring.substring(+startchar, +endchar);
+    charlength = msgstring.length;
+    startchar = endchar + 1;
+    sentchars +=charlength + 1;
+    msgnum += 1;
+    numberfiller = (' [' + msgnum + '/#]');
+    var formattedstring = ('```' + preappend + msgstring + numberfiller + '```');
+    msgarray[msgindex] = formattedstring;
+    msgindex += 1;
+    if (sentchars > totalchars) break;
+  };
+    var msgcount = msgarray.length;
+    for (var i = 0; i < msgarray.length; i++){
+      var segment = msgarray[i];
+      var newsegment = segment.replace('#',msgcount);
+      msgarray[i] = newsegment;
+    };
+    return new Promise(resolve =>{
+      resolve (msgarray);
+    })
+
+  },
+
+  discordmsgparser: async function(a){
+    var firstarray = a;
+    var msgarray = [];
+    var arrayval = 0;
+    for (var i = 0; i < firstarray.length; i++){
+      var messagestring = firstarray[i];
+      if (messagestring === '') continue;
+      var totalchars = messagestring.length;
+      if (messagestring.length < 1994) {
+        msgarray[arrayval] = ('```' + messagestring + '```');
+        arrayval += 1;
+        continue;
+      };
+      var startchar = 0;
+      var maxchars = 1994;
+      var endchar = 0;
+      var sentchars = 0;
+      var charlength = 0;
+      while (sentchars < totalchars ){
+        var maxend = startchar + maxchars;
+        var initialmsgstring = messagestring.substring(startchar, maxend);
+        var lastperiod = initialmsgstring.lastIndexOf(".");
+        var lastquestion = initialmsgstring.lastIndexOf("?");
+        var lastexclaim = initialmsgstring.lastIndexOf("!");
+        var lastquotes = initialmsgstring.lastIndexOf('"');
+        var endindex = Math.max(lastperiod,lastquestion,lastexclaim,lastquotes);
+        if (maxend >= totalchars) endindex = maxend;
+        endchar = +endindex + +startchar + 1;
+        var msgstring = messagestring.substring(+startchar, +endchar);
+        charlength = msgstring.length;
+        startchar = endchar + 1;
+        sentchars +=charlength + 1;
+        var formattedstring = ('```' + msgstring + '```');
+        msgarray[arrayval] = formattedstring;
+        arrayval += 1;
+        if (sentchars >= totalchars) break;
+      };
+      return new Promise(resolve =>{
+        resolve (msgarray);
+      })
+
+    };
+
+  },
+  
+npcattack: async function(a, b){
+    var attacker = a;
+    var target = b;
+    //get attacker's roll data and set attack against target
+    var [stat, attack, bonus, damage] = await this.getval(2, attacker, ['combatstat','atk','bonus', 'damage']);
+    var modifier = await this.getproperty(2, attacker, stat);
+    var [modtotal, newstat] = await this.sumstring(modifier);
+    var [attacktotal, newatk] = await this.sumstring(attack);
+    var [damagetotal, newdmg] = await this.sumstring(damage);
+    var roll = Math.floor(Math.random() * 10) + 1;
+    var total = +roll + +bonus + +modtotal + +attacktotal;
+    if (total <= 1) this.doom(attacker);
+    if (total < 4 ) return;
+    var damageadd = await this.getproperty(4, total, 'dmgtarget');
+    var fulldamage = +damagetotal + +damageadd;
+    var attackerstring = (name + ' | ' + total + ' | ' + fulldamage + ',');
+    var pendingatks = await this.getproperty(2, target, 'attacks');
+    var newstring = pendingatks + attackerstring;
+    this.setval(2, target, ['attacks'], [newstring]);
+    this.setval(2, attacker, [stat, 'atk', 'bonus', 'dmg'],[newstat, newatk, 0, newdmg]);
+},
+  
+npcdefend: async function(a){
+    var searchval = a;
+    var [def, attacks, bonus] = await this.getproperty(2, searchval, ['def', 'attacks', 'bonus']);
+    var attackstring = attacks.split(',');
+    for (var i = 0; i < attackstring.length; i++){
+      var attack = attackstring[i];
+      var [attacker, attackroll, damage] = attack.split(' | ');
+      var roll = Math.floor(Math.random() *10) +1;
+      var total = +roll + +bonus + +def;
+      var damagestring = await this.getproperty(2, searchval, 'damages');
+      var attackstr = (attack + ',');
+      var newattacks = attacks.replace(attack, '');
+      if (+total >= +attackroll) attackstr = '';
+      var newdamages = (damagestring + attackstr);
+      
+      await this.setval(2, searchval, ['attacks', 'damages'], [newattacks, newdamages]);
+    }
+},
+  
+npccharge: async function(a, b){
+    var searchval = a;//NPC doing the charging
+    var target = b;//action being charged
+
+    var roll = Math.floor(Math.random() *10) + 1;
+    var chargeval = await this.getproperty(4, roll, 'chargeval');
+    var statval = await this.getproperty(2, searchval, target);
+    var newstat = (statval + ',' + chargeval);
+    this.setval(2, searchval, [target],[newstat]);
+},
+  
+npcheal: async function(a, b){
+    var searchval = a;
+    var target = b;
+    var heal = await this.getproperty(2, searchval, 'heal');
+    var roll = Math.floor(Math.random() *10) +1;
+    var total = +roll + +heal;
+    var healval = await this.getproperty(4, total, 'healeffect');
+    this.hpresolve(target, healval);
+},
+  
+npccover: async function(a, b){
+    var searchval = a;
+    var target = b;
+
+    var attacks = await this.getproperty(2, target, 'damages');
+    var attackstring = attacks.split(',');
+    for (var i = 0; i < attackstring.length; i++){
+      attack = attackstring[i];
+      var [attacker, attackroll, damage] = attack.split(' | ');
+    }
+},
+
+npcbuff: async function(a, b){
+    var searchval = a;
+    var [target, stat] = b.split('|');
+    var statstring = await this.getproperty(2, target, stat);
+    var buff = await this.getproperty(2, searchval, 'buff');
+    var roll = Math.floor(Math.random() *10) +1;
+    var total = +roll + +buff;
+    var buffval = await this.getproperty(4, total, 'buffval');
+    var newstring = (statstring + ',' + buffval);
+    this.setval(2, target, [stat], [newstring]);
+},
+
+npcanswer: async function(a, b){
+    var searchval = a;
+    var reply = b;
+},
   
 }
